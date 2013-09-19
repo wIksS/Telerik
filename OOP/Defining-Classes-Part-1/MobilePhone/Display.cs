9536ff9391af688695ad193759701da17ecc0ffe
wIksS <viktordakov97@gmail.com>
2013-09-19 21:49:10 +0300

    OOP
    First  Homework

---
+++ OOP/Defining-Classes-Part-1/MobilePhone/Display.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    class Display
+    {
+        private int size;
+        private int colorsNumber;
+        //declarating fields
+
+        public Display(int size = 0 , int colorsNumber = 0)
+        {
+            this.size = size;
+            this.colorsNumber = colorsNumber;
+        }
+        // making the constructor taking parameters or making them null
+
+        public int Size
+        {
+            get { return this.size; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Incorrect size !");
+                }
+                // checking if the size is null or empty
+                this.size = value;
+            }
+        }
+        // making it public so it can be adjusted by the outside
+
+        public int ColorsNumber
+        {
+            get { return this.colorsNumber; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Incorrect ColorsNumber !");
+                }
+                // checking if the ColorsNumber is null or empty
+                this.ColorsNumber = value;
+            }
+        }
+        // making it public so it can be adjusted by the outside
+    }
+}
---
+++ OOP/Defining-Classes-Part-1/MobilePhone/GSM.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    public class GSM
+    {
+        private string model;
+        private string manufacturer;
+        private decimal price;
+        private string owner;
+        // defining the mobile phone information (fields)
+        static GSM IPhone4S;
+        // defining the IPhone4S field
+        private List<Call> callHistory = new List<Call>();
+
+        public GSM(string model = null, string manufacturer = null, decimal price  = 0, string owner = null)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+            this.owner = owner;
+        }
+        // making the constructor taking parameters or making them null
+
+        public List<Call> CallHistory
+        {
+            get { return this.callHistory; }
+            // I return the private callHistroy
+            set { this.callHistory = value; }
+            // I make a new callHistory
+        }
+
+        public void AddCall(string dialedNumber = "09999999", int duration = 60)
+        {
+            DateTime now = DateTime.Now;
+            callHistory.Add(new Call(now, dialedNumber, duration));
+            // making a method to add a Call in my List<Calls>
+        }
+
+        public void DeleteCall(int index)
+        {
+            this.callHistory.RemoveAt(index);
+            // this method deletes the element by the given index
+        }
+
+        public void ClearCallHistory()
+        {
+            this.callHistory.Clear();
+            // removing all elements in the List<Calls>
+        }
+
+        public decimal CalculateTotalCallPrice(decimal price)
+        {
+            decimal totalPrice = 0;
+            foreach (var call in callHistory)
+            {
+                double minutes = TimeSpan.FromSeconds(call.Duration).TotalMinutes;
+                // converting seconds to minutes
+                totalPrice += (decimal)minutes * price;
+                // multiplying minutes by price so i can get
+                // the total price for the current call
+            }
+            return totalPrice;
+        }
+    
+        public GSM IPhone4s
+        {
+            get { return IPhone4S; }
+            set { IPhone4S = value; }
+            // Making a property for the IPhone4S
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Incorrect model !");
+                }
+                // checking if the model is null or empty
+                this.model = value;
+            }
+        }
+        // making it public so it can be adjusted by the outside
+
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set 
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Incorrect manufacturer !");
+                }
+                // checking if the manufacturer is null or empty
+                this.manufacturer = value; 
+            }
+        }
+        // making it public so it can be adjusted by the outside
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Incorrect price !");
+                }
+                // checking if the manufacturer is null or empty
+                this.price = value;
+            }
+        }
+        // making it public so it can be adjusted by the outside
+
+        public string Owner
+        {
+            get { return this.owner; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Incorrect owner !");
+                }
+                // checking if the owner is null or empty
+                this.owner = value;
+            }
+        }
+        // making it public so it can be adjusted by the outside
+
+        public override string ToString()
+        {
+            StringBuilder information = new StringBuilder();
+            information.AppendLine("Model -- > " + model);
+            information.AppendLine("Manufacturer -- > " + manufacturer);
+            information.AppendLine("Price -- > " + Price);
+            information.Append("Owner -- > " + Owner);            
+            return information.ToString();
+            // I m putting all of the info in a string with new lines and everything,
+            //so it can look good on the console
+        }
+        //overriding ToString() method
+    }
+}
